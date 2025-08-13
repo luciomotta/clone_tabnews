c4a27b61357c2b0615ac1e104677e136522a6799
Lucio Motta <lucio-hto@hotmail.com>
2025-08-13 08:10:51 -0300

    Testar iniciar server no Workspace online GITHUB e deixar acessivel a acessos globais

---
+++ pages/index.js
@@ -0,0 +1,6 @@
+function Home() { // esse e um componente React que representa a página inicial do aplicativo
+  // O componente retorna um elemento JSX que renderiza um título
+    return <h1>Bem-vindo ao Clone do TabNews</h1>;
+}
+ 
+export default Home; // exporta o componente Home como o padrão do módulo, permitindo que seja importado em outros arquivos

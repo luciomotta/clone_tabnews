a7abd03364696917c32a316062f75b55da2a2f13
luciomotta <lucio-hto@hotmail.com>
2025-08-15 15:44:04 -0300

    Testando DEPLOY no VERSEL

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 function Home() { // esse e um componente React que representa a página inicial do aplicativo
   // O componente retorna um elemento JSX que renderiza um título
-    return <h1>Bem-vindo ao Clone do TabNews</h1>;
+    return <h1>Bem-vindo ao Clone do TabNews!!!!!</h1>;
+
 }
+
+
  
 export default Home; // exporta o componente Home como o padrão do módulo, permitindo que seja importado em outros arquivos
